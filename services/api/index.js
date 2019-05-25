39e4770409f10d0fa016a67a4f05488cd45876a8
edus44-ggg <eduardo.hidalgo@gigigo.com>
2019-05-26 01:37:37 +0200

    optim

--- services/api/index.js
+++ services/api/index.js
@@ -1,8 +1,9 @@
 const {get} = require('@package/models')
+const {set} = require('@package/tools')
 const app = require('express')()
 
 app.use((req,res)=>{
-  res.send({api:1,msg:get()})
+  res.send({api:1,msg:get()+set()})
 })
 
 app.listen(5000,()=>{
