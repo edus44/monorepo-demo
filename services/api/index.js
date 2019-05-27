aba772316f37b9d628a66a900e38d101ace86298
edus44-ggg <eduardo.hidalgo@gigigo.com>
2019-05-27 23:44:42 +0200

    rev1

--- services/api/index.js
+++ services/api/index.js
@@ -2,10 +2,11 @@ const {get} = require('@package/models')
 const {set} = require('@package/tools')
 const app = require('express')()
 
+console.log('rev1')
+
 app.use((req,res)=>{
   res.send({api:1,msg:get()+set()})
 })
-
 app.listen(5000,()=>{
   console.log('listening 5000')
 })
