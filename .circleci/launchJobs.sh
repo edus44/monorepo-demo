bbeab1e288e639b30ea8e22e6c9e25e67e80cd0b
edus44-ggg <eduardo.hidalgo@gigigo.com>
2019-06-08 12:29:43 +0200

    test

--- .circleci/launchJobs.sh
+++ .circleci/launchJobs.sh
@@ -6,7 +6,6 @@ for serviceName in $DEPLOY_SERVICES_NAMES; do
   echo $serviceName
   curl -s -u $CIRCLE_TOKEN: \
     -d build_parameters[CIRCLE_JOB]=$serviceName \
-    -d build_parameters[CIRCLE_WORKFLOW_ID]=$CIRCLE_WORKFLOW_ID \
     -d revision=$CIRCLE_SHA1 \
     https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
 done
