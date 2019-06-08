008cea4ca51d226ff251e1bfa16c7ab879972fd8
edus44-ggg <eduardo.hidalgo@gigigo.com>
2019-06-08 12:34:49 +0200

    test

--- .circleci/launchJobs.sh
+++ .circleci/launchJobs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo $DEPLOY_SERVICES_NAMES;
+apk add jq
 
 for serviceName in $DEPLOY_SERVICES_NAMES; do
-  echo $serviceName
-  curl -s -u $CIRCLE_TOKEN: \
+  echo "Launching $serviceName"
+  result=$(curl -s -u $CIRCLE_TOKEN: \
     -d build_parameters[CIRCLE_JOB]=$serviceName \
     -d revision=$CIRCLE_SHA1 \
-    https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
+    https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH)
+  buildUrl=$( echo $RES | jq -r ".build_url" )
+  echo $buildUrl
 done
