50880bfb2ad37fd2a387add69b2adff83df6f640
edus44-ggg <eduardo.hidalgo@gigigo.com>
2019-06-08 12:53:42 +0200

    test

--- .circleci/launchJobs.sh
+++ .circleci/launchJobs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 for serviceName in $DEPLOY_SERVICES_NAMES; do
-  echo "Launching $serviceName"
+  echo "Launching: $serviceName"
+
   result=$(curl -s -u $CIRCLE_TOKEN: \
     -d build_parameters[CIRCLE_JOB]=$serviceName \
-    -d revision=$CIRCLE_SHA1 \
     https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH)
-  buildUrl=$( echo $RES | jq -r ".build_url" )
-  echo $buildUrl
+
+  # Build url
+  buildUrl=$( echo $result | jq -r ".build_url" )
+  echo "Build URL: $buildUrl"
+
+  # Save artifact link
+  mkdir -p /tmp/artifacts
+  echo "<meta http-equiv='refresh' content='0; url=$buildUrl'>" > /tmp/artifacts/$serviceName.html
 done
