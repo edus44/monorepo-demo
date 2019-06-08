62bffc812e8d8c1235b764a7808b2481b93444a8
edus44-ggg <eduardo.hidalgo@gigigo.com>
2019-06-08 11:19:53 +0200

    test

---
+++ .circleci/changeDetect.sh
@@ -0,0 +1,23 @@
+set +e
+servicesNames=$(find services -mindepth 1 -maxdepth 1 -type d | cut -d/ -f2)
+changedPaths=$(git diff $COMMIT_RANGE  --name-only | cut -d/ -f1,2 | sort -u)
+changedBase=$(echo $changedPaths | tr ' ' '\n' | grep -E "^(.circleci|packages)/" )
+changedServices=$(echo $changedPaths | tr ' ' '\n' | grep services/ | cut -d/ -f2)
+set -e
+
+echo " Services names   => $(echo $servicesNames)"
+echo " Commit range     => $(echo $COMMIT_RANGE)"
+echo " Changed paths    => $(echo $changedPaths)"
+echo " Changed base     => $(echo $changedBase)"
+echo " Changed services => $(echo $changedServices)"
+echo ""
+
+if [[ $changedBase = '' ]]; then
+  echo 'No changes in base dependencies'
+  deployServicesNames=$changedServices
+else
+  echo "Found changes in base dependencies"
+  deployServicesNames=$servicesNames
+fi
+
+echo "Deploying services: $(echo $deployServicesNames)"
